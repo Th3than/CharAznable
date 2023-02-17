9f3dc8da9f0dcec79490b04228b0264bae2439cf
Natan <Th3than@gmail.com>
2023-02-17 17:00:19 +0700

    lab19-1

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,21 +20,75 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string fname,vector<string> &iname,vector<int> &iscores,vector<char> &igrades){
+    ifstream data("name_score.txt");
+    string smass;
+    int sor,p1,p2,p3;
+    char na[100];
+    char form[] = "%[^:]: %d %d %d";
+    while (getline(data,smass))
+    {
+        sscanf(smass.c_str(),form,na,&p1,&p2,&p3);
+        sor = p1+p2+p3;
+        iname.push_back(na);
+        iscores.push_back(sor);
+        igrades.push_back(score2grade(sor));
+    }
+    data.close();
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+    string text;
+    char a[100];
+    char b[100];
+    char format[] = "%s %[^\n]";
+    cout << "Please input your command: ";
+    getline(cin,text);
+    sscanf(text.c_str(),format,&a,&b);
+    command = a;
+    key = b;
 }
 
-void searchName(){
-
+void searchName(vector<string> nname,vector<int> nscores,vector<char> ngrades,string nkey){
+    bool fin = false;
+    cout << "---------------------------------"<<'\n';
+    for (unsigned int i = 0; i < nname.size(); i++)
+    {
+        
+        if (nkey == toUpperStr(nname[i]))
+        { 
+            cout << nname[i] << "'s score = " << nscores[i] << '\n';
+            cout << nname[i] << "'s grade = " << ngrades[i]<<'\n';      
+            fin = true;
+        }
+        
+    }
+    if (!fin)
+    {
+        cout << "Cannot found."<<'\n';
+    }
+    cout << "---------------------------------"<<'\n';
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> gname,vector<int> gscores,vector<char> ggrades,string gkey){
+     bool fig = false;
+     cout << "---------------------------------"<<'\n';
+    for (unsigned int i = 0; i < gname.size(); i++)
+    { 
+        if (gkey[0]  == ggrades[i])
+        {
+            cout << gname[i] << " (" << gscores[i]<<")"<<'\n';
+            fig = true;
+        }
+        
+    }
+    if (!fig)
+    {
+        cout << "Cannot found."<<'\n';
+    }
+    cout << "---------------------------------"<<'\n';
 }
+
 
 
 int main(){
